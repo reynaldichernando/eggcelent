c912f4c2979785bc1aa4d81c9ed26a41f9c62192
reynaldichernando <reynaldichernando@gmail.com>
2021-03-29 04:10:32 +0700

    Hide drop down menu on click

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,12 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Dropdown = ({isOpen, toggle}) => {
+
+    const handleLinkClick = () => {
+        if(isOpen) toggle();
+    }
+
     return (
         <div className={isOpen ? 'grid grid-rows-4 text-center items-center bg-yellow-500' : 'hidden'}>
-            <Link className="p-4" to="/">Home</Link>
-            <Link className="p-4" to="/menu">Menu</Link>
-            <Link className="p-4" to="/about">About</Link>
-            <Link className="p-4" to="/contact">Contact</Link>
+            <Link className="p-4" to="/" onClick={handleLinkClick}>Home</Link>
+            <Link className="p-4" to="/menu" onClick={handleLinkClick}>Menu</Link>
+            <Link className="p-4" to="/about" onClick={handleLinkClick}>About</Link>
+            <Link className="p-4" to="/contact" onClick={handleLinkClick}>Contact</Link>
         </div>
     )
 }
