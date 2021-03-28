69ceb138035354671d3fa9d1186c238b6e6c13c4
reynaldichernando <reynaldichernando@gmail.com>
2021-03-29 04:14:39 +0700

    Change footer text size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="flex justify-center items-center h-16 bg-black text-white">
-            <p>Copyright &copy; 2021 EGG All rights reserved.</p>
+            <p className="text-sm">Copyright &copy; 2021 EGG All rights reserved.</p>
         </div>
     )
 }
